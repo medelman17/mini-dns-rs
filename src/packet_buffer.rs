bcbf5830a022f22fbd2ef5cd77e02ec20a413826
Michael Edelman <michael@Michaels-MacBook-Pro.local>
2022-12-19 13:49:40 -0700

    read and write dns packets

---
+++ src/packet_buffer.rs
@@ -0,0 +1,169 @@
+use crate::errors::PacketBufferError;
+
+type Result<R> = std::result::Result<R, PacketBufferError>;
+
+pub struct BytePacketBuffer {
+    pub buf: [u8; 512],
+    pub pos: usize,
+}
+
+impl BytePacketBuffer {
+    /// Create a fresh buffer for holding packet contents
+    pub fn new() -> BytePacketBuffer {
+        BytePacketBuffer {
+            buf: [0; 512],
+            pos: 0,
+        }
+    }
+
+    /// Current position within buffer
+    pub fn pos(&self) -> usize {
+        self.pos
+    }
+
+    /// Step buffer position forward a specific number of steps
+    pub fn step(&mut self, steps: usize) -> Result<()> {
+        self.pos += steps;
+        Ok(())
+    }
+
+    /// Change the buffer position
+    fn seek(&mut self, pos: usize) -> Result<()> {
+        self.pos = pos;
+        Ok(())
+    }
+
+    fn read(&mut self) -> Result<u8> {
+        if self.pos >= 512 {
+            return Err(PacketBufferError::EndOfBuffer);
+        }
+        let res = self.buf[self.pos];
+        self.pos += 1;
+        Ok(res)
+    }
+
+    fn get(&mut self, pos: usize) -> Result<u8> {
+        if pos >= 512 {
+            return Err(PacketBufferError::EndOfBuffer);
+        }
+        Ok(self.buf[pos])
+    }
+
+    fn get_range(&mut self, start: usize, len: usize) -> Result<&[u8]> {
+        if start + len >= 512 {
+            return Err(PacketBufferError::EndOfBuffer);
+        }
+        Ok(&self.buf[start..start + len as usize])
+    }
+
+    /// Read two bytes; move 2 steps forward
+    pub fn read_u16(&mut self) -> Result<u16> {
+        let res = ((self.read()? as u16) << 8) | (self.read()? as u16);
+
+        Ok(res)
+    }
+
+    /// Read four bytes; move 4 steps forward
+    pub fn read_u32(&mut self) -> Result<u32> {
+        let res = ((self.read()? as u32) << 24)
+            | ((self.read()? as u32) << 16)
+            | ((self.read()? as u32) << 8)
+            | ((self.read()? as u32) << 0);
+
+        Ok(res)
+    }
+
+    pub fn read_qname(&mut self, outstr: &mut String) -> Result<()> {
+        let mut pos = self.pos();
+        let mut jumped = false;
+        let max_jumps = 5;
+        let mut jumps_performed = 0;
+        let mut delim = "";
+        loop {
+            if jumps_performed > max_jumps {
+                return Err(PacketBufferError::JumpLimitExceeded);
+            }
+            let len = self.get(pos)?;
+            if (len & 0xC0) == 0xC0 {
+                if !jumped {
+                    self.seek(pos + 2)?;
+                }
+
+                let b2 = self.get(pos + 1)? as u16;
+                let offset = (((len as u16) ^ 0xC0) << 8) | b2;
+                pos = offset as usize;
+
+                jumped = true;
+                jumps_performed += 1;
+                continue;
+            } else {
+                pos += 1;
+                if len == 0 {
+                    break;
+                }
+
+                outstr.push_str(delim);
+                // Extract the actual ASCII bytes for this label and append them
+                // to the output buffer.
+                let str_buffer = self.get_range(pos, len as usize)?;
+                outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+
+                delim = ".";
+
+                // Move forward the full length of the label.
+                pos += len as usize;
+            }
+        }
+
+        if !jumped {
+            self.seek(pos)?;
+        }
+        Ok(())
+    }
+
+    pub fn write(&mut self, val: u8) -> Result<()> {
+        if self.pos >= 512 {
+            return Err(PacketBufferError::EndOfBuffer);
+        }
+        self.buf[self.pos] = val;
+        self.pos += 1;
+        Ok(())
+    }
+
+    pub fn write_u8(&mut self, val: u8) -> Result<()> {
+        self.write(val)?;
+        Ok(())
+    }
+
+    pub fn write_u16(&mut self, val: u16) -> Result<()> {
+        self.write((val >> 8) as u8)?;
+        self.write((val & 0xFF) as u8)?;
+        Ok(())
+    }
+
+    pub fn write_u32(&mut self, val: u32) -> Result<()> {
+        self.write(((val >> 24) & 0xFF) as u8)?;
+        self.write(((val >> 16) & 0xFF) as u8)?;
+        self.write(((val >> 8) & 0xFF) as u8)?;
+        self.write(((val >> 0) & 0xFF) as u8)?;
+        Ok(())
+    }
+
+    pub fn write_qname(&mut self, qname: &str) -> Result<()> {
+        for label in qname.split('.') {
+            let len = label.len();
+            if len > 0x3f {
+                return Err(PacketBufferError::LabelTooBig);
+            }
+
+            self.write_u8(len as u8)?;
+            for b in label.as_bytes() {
+                self.write_u8(*b)?;
+            }
+        }
+
+        self.write_u8(0)?;
+
+        Ok(())
+    }
+}
